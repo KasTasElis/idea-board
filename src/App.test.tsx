109dd2b11e01a2c1269b74f145504d1f33116de1
EliTamošauskas <tamosauskas09@gmail.com>
2023-07-18 10:03:41 +0300

    feat: add user events lib

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,20 @@
 import { render, screen } from "./react-testing-lib/test-utils";
+import userEvent from "@testing-library/user-event";
+
+const user = userEvent.setup();
 
 import App from "./App";
 
 describe("App", () => {
-  it("renders headline", () => {
+  it("renders the app", async () => {
     render(<App />);
+
+    const addIdeaBtn = screen.getByText(/Add New Idea/i);
+    expect(addIdeaBtn).toBeInTheDocument();
+
+    screen.debug();
+
+    await user.click(addIdeaBtn); // Perform a click with `shiftKey: true`
 
     screen.debug();
   });
